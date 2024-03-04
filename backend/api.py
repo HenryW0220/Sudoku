87b85f0c81c4b8a362633378da68e4d3f77debc6
Katherine Lesavich <klesavich@wisc.edu>
2024-03-04 16:27:47 -0600

    Updated outline of endpoint code, added comments

--- backend/api.py
+++ backend/api.py
@@ -2,7 +2,22 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+'''
+    GET endpoint to retrieve a user's Sudoku board from the MySQL database.
+'''
+@app.get('/boards/retrieve_board')
+def retrieve_board():
+    # Get it with the board ID
+    # List of numbers?
+    # Parse and return
+    pass
 
-@app.route('/endpoint', methods=['GET'])
-def get_data():
-    return 'Success!'
+'''
+    POST endpoint to store a user's Sudoku board in the MySQL database.
+'''
+@app.post('/boards/store_board')
+def store_board():
+    # Given a list of numbers
+    # Convert to a string of numbers
+    # Store with unique board ID
+    pass
