e7744ac7690e98c55a589cd95163dd1218f7fdde
Katherine Lesavich <klesavich@wisc.edu>
2024-03-07 11:34:51 -0600

    Finished POST endpoint

--- backend/api.py
+++ backend/api.py
@@ -18,7 +18,6 @@ class SudokuBoard(db.Model):
 def retrieve_board(board_id):
     # Retrieve row of DB whose id is equal to that of board_id
     sudoku_board = SudokuBoard.query.filter_by(id=board_id).first()
-
     if sudoku_board:
         # Access board column of that row to split string of numbers and convert to ints all into a list
         board_numbers = [int(num) for num in sudoku_board.board.split()]
@@ -28,17 +27,21 @@ def retrieve_board(board_id):
         return 'Invalid Board ID: Board Not Found', 404
 
 # POST endpoint to store a user's Sudoku board in the MySQL database.
-@app.post('/boards/store_board')
-def store_board():
-    # Given a list of numbers (from the request body?)
-    board_numbers = request.json.get('numbers')
-    # Generate a board ID? or is it given?
+@app.post('/boards/store_board/<int:board_id>')
+def store_board(board_id):
+    # Grab the board numbers from request body
+    board_numbers = request.json.get('board_numbers')
+    if board_numbers:
+        # Convert the list of numbers back to a string
+        board_numbers = ' '.join(str(num) for num in board_numbers)
+        # Create a new instance of the board
+        sudoku_board = SudokuBoard(id=board_id, board=board_numbers)
+        # Store it in the database
+        db.session.add(sudoku_board)
+        db.session.commit()
+        return f'Sudoku Board (id: {board_id}) Stored Successfully!'
+    else:
+        return 'Board Numbers Not Provided: Bad Board Request', 400
 
-    # Convert to a string of numbers
-
-    # Create a new instance of the board
-    # Store it in the database
-    return 'Board stored successfully with id _'
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
