53ffaa9f89f3d451efbcd17104577ea32d80f117
Katherine Lesavich <klesavich@wisc.edu>
2024-04-07 16:33:30 -0500

    POST endpoint tests working

--- backend/app/app_test.py
+++ backend/app/app_test.py
@@ -2,13 +2,27 @@ import pytest
 from app import app
 from unittest.mock import MagicMock
 
-# # Mocking the database connection function
-# def mock_get_db_connection():
-#     connection = MagicMock()
-#     cursor = MagicMock()
-#     cursor.fetchone.return_value = ('1000', '1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9')
-#     # cursor.fetchone.return_value = None
-#     return connection, cursor
+# Define the mock_db_connection function to create a closure
+# This allows us to create different instances of mock_get_db_connection with a different fetchone_return_value to customize its behavior for different test cases
+def mock_get_db_connection(fetchone_return_value):
+    def mock_db_connection():
+        connection = MagicMock()
+        cursor = MagicMock()
+        cursor.fetchone.return_value = fetchone_return_value
+        return connection, cursor
+    return mock_db_connection
+
+def mock_post_db_connection(fetchone_return_value):
+    def mock_db_connection():
+        connection = MagicMock()
+        cursor = MagicMock()
+        # Mocking the execution of the SQL insert query
+        cursor.execute.return_value = None
+        # Assuming you want to simulate a successful insertion
+        # You can simulate the behavior of fetching the inserted data, if necessary
+        cursor.fetchone.return_value = fetchone_return_value
+        return connection, cursor
+    return mock_db_connection
 
 # Set up a fixture for the app client
 @pytest.fixture
@@ -19,44 +33,27 @@ def client():
 
 # Test cases
 def test_retrieve_board_found(client, monkeypatch):
-    # Mocking the database connection function
-    def mock_get_db_connection():
-        connection = MagicMock()
-        cursor = MagicMock()
-        cursor.fetchone.return_value = ('1000', '1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9')
-        # cursor.fetchone.return_value = None
-        return connection, cursor
-
-    # Mocking the database connection function
-    monkeypatch.setattr('app.get_db_connection', mock_get_db_connection)
-
     # Simulate a board found scenario
     expected_board_id = 1000
     expected_board_contents = '1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9'
     expected_response = [expected_board_id] + [int(num) for num in expected_board_contents.split()]
 
+    # Mocking the database connection function
+    monkeypatch.setattr('app.get_db_connection', mock_get_db_connection((str(expected_board_id), expected_board_contents)))
+
     # Make a request to the endpoint
     response = client.get(f'/boards/retrieve_board/{expected_board_id}')
-    print(response.json)
 
     # Assert response
     assert response.status_code == 200
     assert response.json == expected_response
 
 def test_retrieve_board_not_found(client, monkeypatch):
-    # Mocking the database connection function
-    def mock_get_db_connection():
-        connection = MagicMock()
-        cursor = MagicMock()
-        cursor.fetchone.return_value = None
-        return connection, cursor
-    
-    # Mocking the database connection function
-    monkeypatch.setattr('app.get_db_connection', mock_get_db_connection)
-    
-
     # Simulate a board not found scenario
     non_existent_board_id = 999
+
+    # Mocking the database connection function
+    monkeypatch.setattr('app.get_db_connection', mock_get_db_connection(None))
 
     # Make a request to the endpoint
     response = client.get(f'/boards/retrieve_board/{non_existent_board_id}')
@@ -65,3 +62,32 @@ def test_retrieve_board_not_found(client, monkeypatch):
     assert response.status_code == 404
     assert response.json == {'message': 'Invalid Board ID: Board Not Found'}
 
+def test_store_board_success(client, monkeypatch):
+    # Simulate a store board scenario
+    board_id = 9999
+    board_contents = '0 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9 1 2 3 4 5 6 7 8 9'
+
+    # Mocking the database connection function
+    monkeypatch.setattr('app.get_db_connection', mock_post_db_connection(f'Sudoku Board (id: {board_id}) Stored Successfully!'))
+
+    # Make a request to the endpoint
+    response = client.post(f'/boards/store_board/{board_id}', json={'board_contents': board_contents})
+
+    # Assert response
+    assert response.status_code == 200
+    assert response.data == f'Sudoku Board (id: {board_id}) Stored Successfully!'.encode('utf-8')
+
+def test_store_board_unsuccess(client, monkeypatch):
+    # Simulate a store board scenario
+    board_id = 9999
+    board_contents = ''
+
+    # Mocking the database connection function
+    monkeypatch.setattr('app.get_db_connection', mock_post_db_connection('Board Numbers Not Provided: Bad Board Request'))
+
+    # Make a request to the endpoint
+    response = client.post(f'/boards/store_board/{board_id}', json={'board_contents': board_contents})
+
+    # Assert response
+    assert response.status_code == 400
+    assert response.json == {'message': 'Board Numbers Not Provided: Bad Board Request'}
