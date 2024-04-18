eabf3ba79c7f258d5c061ecf856170fd75823db0
Katherine <klesavich@wisc.edu>
2024-04-18 11:55:31 -0500

    Retrieving and displaying boards

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
 import FullSudokuGrid from './components/FullSudokuGrid';
+import MainMenu from './components/MainMenu';
+
 function App() {
   return <div className={"bg-indigo-900 h-screen w-full flex flex-col items-center"}>
       <p className={"font-bold text-5xl text-neutral-200 p-10"}>Psudoku</p>
-      <FullSudokuGrid/>
+      <MainMenu/>
     </div>
 }
 export default App;
--- frontend/src/components/MainMenu.tsx
+++ frontend/src/components/MainMenu.tsx
@@ -1,2 +1,25 @@
 import React, { useEffect, useState } from "react";
 
+export default function MainMenu() {
+
+  const [boards, setBoards] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5002/boards/retrieve_all_boards')
+    .then(res => res.json())
+    .then(json => {
+      setBoards(json)
+    })
+  }, []);
+
+  return <>
+    <h1>Welcome!</h1>
+    <h1>Select a Game Board:</h1>
+    {
+      boards.map(board => {
+        return board
+      })
+    }
+  </>
+  
+}
