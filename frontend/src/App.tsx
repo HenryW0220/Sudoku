1af3ec75b93c09331556ccc9d39311c775d1d5bc
Katherine <klesavich@wisc.edu>
2024-04-21 21:23:35 -0500

    Added buttons

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
 import './App.css'
 import FullSudokuGrid from './components/FullSudokuGrid';
 import MainMenu from './components/MainMenu';
+import { AuthForm } from './components/AuthForm';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return <div className={"bg-indigo-900 h-screen w-full flex flex-col items-center"}>
       <p className={"font-bold text-5xl text-neutral-200 p-10"}>Psudoku</p>
+      {/* <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<AuthForm />}></Route>
+          <Route path="/boards/retrieve_all_boards" element={<MainMenu />}></Route>
+          <Route path="/boards/retrieve_board/1002" element={<FullSudokuGrid />}></Route>
+        </Routes>
+      </BrowserRouter> */}
       <MainMenu/>
     </div>
 }
--- frontend/src/components/MainMenu.tsx
+++ frontend/src/components/MainMenu.tsx
@@ -1,8 +1,15 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from 'react-router';
 
 export default function MainMenu() {
 
   const [boards, setBoards] = useState([]);
+  const [testBoards, setTestBoards] = useState([
+    [1005, [1, 2, 3], [1, 2, 3]],
+    [1006, [4, 5, 6], [4, 5, 6]],
+    [1007, [8, 9, 0], [8, 9, 0]]
+  ]);
+  // const navigate = useNavigate()
 
   useEffect(() => {
     fetch('http://localhost:5002/boards/retrieve_all_boards')
@@ -12,14 +19,20 @@ export default function MainMenu() {
     })
   }, []);
 
+  // const handleClick = () => {
+  //   navigate(`http://localhost:5002/boards/retrieve_board/1002`)
+  // }
+
   return <>
     <h1>Welcome!</h1>
     <h1>Select a Game Board:</h1>
-    {
-      boards.map(board => {
-        return board
-      })
-    }
+    <div style={{ display: "flex", flexDirection: "row" }}>
+        {
+          testBoards.map(board => ( 
+            <button >{board[0]}</button> )
+          )
+        }
+    </div>
   </>
   
 }
