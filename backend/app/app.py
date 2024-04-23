796f4b9507ef7f61a6abcc852dbe2614fefa4263
Andrew Logan <allogan2@wisc.edu>
2024-04-23 12:28:36 -0500

    updated endpoints

--- backend/app/app.py
+++ backend/app/app.py
@@ -32,13 +32,13 @@ def get_db_connection():
 """
 
 
-@app.get('/boards/retrieve_all_boards')
-def retrieve_all_boards():
+@app.get('/boards/retrieve_all_board_ids')
+def retrieve_all_board_ids():
     """
-    This function handles the GET request to retrieve all boards from the database.
+    This function handles the GET request to retrieve all board ids from the database.
 
     Returns:
-    JSON: A JSON response containing all boards in the database.
+    JSON: A JSON response containing all board ids in the database.
     """
     # Connect to MySQL DB
     connection, cursor = get_db_connection()
@@ -48,8 +48,7 @@ def retrieve_all_boards():
     connection.close()
 
     # Parse results into list of lists with id followed by contents
-    boards_list = [(board[0], [int(num) for num in board[1].split()], [
-                    int(num) for num in board[2].split()]) for board in boards]
+    boards_list = [board[0] for board in boards]
 
     response = jsonify(boards_list)
     response.headers.add('Access-Control-Allow-Origin', '*')
@@ -84,7 +83,7 @@ def retrieve_board(board_id):
         board_id, board_contents, board_answer = result
 
         board_contents = [int(num)
-                                  for num in board_contents.split()]
+                          for num in board_contents.split()]
         board_answer = [int(num) for num in board_answer.split()]
 
         response_data = {
@@ -97,6 +96,30 @@ def retrieve_board(board_id):
         return response
     else:
         return jsonify({'message': 'Invalid Board ID: Board Not Found'}), 404
+
+
+@app.get('/users/<int:user_id>/partial_boards/retrieve_all_partial_board_ids')
+def retrieve_all_partial_board_ids(user_id):
+    """
+    This function handles the GET request to retrieve all partial board ids from the database.
+
+    Returns:
+    JSON: A JSON response containing all partial board ids in the database.
+    """
+    # Connect to MySQL DB
+    connection, cursor = get_db_connection()
+    cursor.execute('USE TEST_DB')
+    cursor.execute('SELECT * FROM PartialBoard')
+    boards = cursor.fetchall()
+    connection.close()
+
+    # Format results into list of dictionaries
+    boards_list = [{'user_id': board[0], 'board_id': board[1]}
+                   for board in boards]
+
+    response = jsonify(boards_list)
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 
 @app.get('/users/<int:user_id>/partial_boards/retrieve_partial_board/<int:board_id>')
