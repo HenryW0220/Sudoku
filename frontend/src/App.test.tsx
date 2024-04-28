c8fcfc5f1491a125c8845d946d62c8f375e17a64
Henry <xhweng@wisc.edu>
2024-04-28 02:02:48 -0500

    Refactor component imports in App.test.tsx

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,16 +2,15 @@ import React from "react";
 import { render, fireEvent, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import App from "./App";
-import useAuth from "./hooks/useAuth";
+import { useAuth } from "./hooks/useAuth";
 import FullSudokuGrid from "./components/FullSudokuGrid";
 import AuthForm from "./components/AuthForm";
 import HeaderBar from "./components/HeaderBar";
 import Note from "./components/Note";
 
 // Mock hooks and components to isolate the tests from external dependencies
-jest.mock('./hooks/useAuth', () => ({
-  __esModule: true,
-  default: jest.fn(), // Mocking the default export
+jest.mock("./hooks/useAuth", () => ({
+  useAuth: jest.fn(),
 }));
 jest.mock("./components/FullSudokuGrid", () => () => <div>FullSudokuGrid</div>);
 jest.mock("./components/AuthForm", () => () => (
